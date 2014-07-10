1163ecede0c7bbe252a69998fd1ad2d3fef6e55e
stephenwang1011 <398733146@qq.com>
2014-07-10 11:51:56 +0800

    alert提示修改一样

--- logistics/src/test/java/com/incito/logistics/pages/pageshelper/AlertPageHelper.java
+++ logistics/src/test/java/com/incito/logistics/pages/pageshelper/AlertPageHelper.java
@@ -6,13 +6,13 @@ public class AlertPageHelper {
 	
 	/**登陆时候用户名和密码都不填写应该弹出，请用此方法验证alert信息*/
 	public static void isAlertUsername(SeleniumUtil seleniumUtil,int waitMillisecondsForAlert){
-		seleniumUtil.isTextEquals(seleniumUtil.switchToPromptedAlertAfterWait(waitMillisecondsForAlert).getText(),"请输入用户名！");
+		seleniumUtil.isTextEquals(seleniumUtil.switchToPromptedAlertAfterWait(waitMillisecondsForAlert).getText(),"用户名或密码错误,请重新登录!");
 	}
 	
 	/**登陆时候只输入用户名，密码都不填写应该弹出，请用此方法验证alert信息*/
 	public static void isAlertPassword(SeleniumUtil seleniumUtil,int waitMillisecondsForAlert){
 		
-		seleniumUtil.isTextEquals(seleniumUtil.switchToPromptedAlertAfterWait(waitMillisecondsForAlert).getText(),"请输入密码！");
+		seleniumUtil.isTextEquals(seleniumUtil.switchToPromptedAlertAfterWait(waitMillisecondsForAlert).getText(),"用户名或密码错误,请重新登录!");
 	}
 
 	/**登陆时候输入错误用户名，密码填写正确，请用此方法验证alert信息*/
