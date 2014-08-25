cd192784b6bee4e81b2da0c9ffb8c6e8745e8fbe
wangyang <398733146@qq.com>
2014-08-25 17:29:32 +0800

    新增注册页面

---
+++ logistics/src/test/java/com/incito/logistics/pages/RegisterPage.java
@@ -0,0 +1,15 @@
+package com.incito.logistics.pages;
+import org.openqa.selenium.By;
+
+/**
+ * 声明登陆界面页面元素
+ * 
+ * */
+public class RegisterPage {
+	/**登陆页面上的：用户名输入框*/
+	public static final By RP_INPUT_USERNAME = By.id("u");
+
+
+	
+
+}
