4ad1c2100005f09a4dd51803c44a9da59c6d06e4
wangyang <398733146@qq.com>
2014-08-25 18:01:32 +0800

    增加元素定位

--- logistics/src/test/java/com/incito/logistics/pages/RegisterPage.java
+++ logistics/src/test/java/com/incito/logistics/pages/RegisterPage.java
@@ -6,8 +6,16 @@ import org.openqa.selenium.By;
  * 
  * */
 public class RegisterPage {
-	/**登陆页面上的：用户名输入框*/
-	public static final By RP_INPUT_USERNAME = By.id("u");
+	/**注册页面上的用户名*/
+	public static final By RP_INPUT_USERNAME = By.id("username");
+	/**注册页面上的密码*/
+	public static final By RP_INPUT_PASSWD = By.id("pwd1");
+	/**注册页面上的确认密码*/
+	public static final By RP_INPUT_REPASSWD = By.id("pwd2");
+	/**注册页面上的注册按钮*/
+	public static final By RP_BUTTON_REGISTER = By.xpath("//*[text()='注册']");
+	/**注册页面上的返回按钮*/
+	public static final By RP_BUTTON_BACK = By.xpath("//*[text()='返回']");
 
 
 	
